6faabb63a829f2f603013b0da4f60fc1c997d8e5
Gerrit Jansen van Vuuren <gerritjvv@gmail.com>
2019-06-30 09:44:15 +0200

    add javac target options

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.gerritjvv/codex "0.1.0"
+(defproject com.github.gerritjvv/codex "0.1.1"
   :description "Simple fast library that compress, encrypt and encode data, for session storage and other use-cases"
   :url "https://github.com/gerritjvv/codex"
   :license {:name "Apache License 2.0"
@@ -7,6 +7,7 @@
   :source-paths      ["src/clojure"]
   :java-source-paths ["src/java"]
 
+  :javac-options     ["-target" "1.8" "-source" "1.8"]
   :dependencies [[org.clojure/clojure "1.10.0-alpha6"]
                  [com.github.gerritjvv/encode-core "1.1.0"]]
 
