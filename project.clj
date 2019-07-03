6af531800f179543257faa043a3e0d47d2ed7fa9
Gerrit Jansen van Vuuren <gerritjvv@gmail.com>
2019-07-04 00:53:38 +0200

    update encode-core deps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.gerritjvv/codex "1.2.0"
+(defproject com.github.gerritjvv/codex "1.2.1"
   :description "Simple fast library that compress, encrypt and encode data, for session storage and other use-cases"
   :url "https://github.com/gerritjvv/codex"
   :license {:name "Apache License 2.0"
@@ -7,9 +7,13 @@
   :source-paths      ["src/clojure"]
   :java-source-paths ["src/java"]
 
+  :global-vars {*warn-on-reflection* true
+                *assert*             false}
+
+
   :javac-options     ["-target" "1.8" "-source" "1.8"]
   :dependencies [[org.clojure/clojure "1.10.0-alpha6"]
-                 [com.github.gerritjvv/encode-core "1.2.0"]]
+                 [com.github.gerritjvv/encode-core "1.2.1"]]
 
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]])
